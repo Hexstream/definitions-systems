0c0ffd8a768b542cc4028140c33c0662e53fa451
Jean-Philippe Paradis <hexstream@hexstreamsoft.com>
2019-12-04 22:42:18 -0500

    Trivially reorder some forms.

--- root.lisp
+++ root.lisp
@@ -6,17 +6,17 @@
   ()
   (:default-initargs :explicit-definition-class-p t))
 
-(defvar *root-system* (make-instance 'defsys:standard-root-system :name 'defsys:system))
-
-(defun defsys:root-system ()
-  *root-system*)
-
-(defmethod make-load-form ((root-system defsys:standard-root-system) &optional environment)
-  (declare (ignore environment))
-  '(defsys:root-system))
-
 (defmethod defsys:locate ((system defsys:standard-root-system) definition-name &key (errorp t))
   (declare (ignore errorp))
   (if (eq definition-name 'defsys:system)
       system
       (call-next-method)))
+
+(defmethod make-load-form ((root-system defsys:standard-root-system) &optional environment)
+  (declare (ignore environment))
+  '(defsys:root-system))
+
+(defvar *root-system* (make-instance 'defsys:standard-root-system :name 'defsys:system))
+
+(defun defsys:root-system ()
+  *root-system*)
