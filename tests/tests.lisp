35a84f6dd1966b9f77417db097cf1df5d3152db9
Jean-Philippe Paradis <hexstream@hexstreamsoft.com>
2020-02-01 15:15:11 -0500

    Tests: Start adding some basic tests.

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -3,5 +3,37 @@
 
 (cl:in-package #:definitions-systems_tests)
 
+(defmacro are (comp expected form &optional description &rest format-args)
+  `(is ,comp ,expected (multiple-value-list ,form) ,description ,@format-args))
+
 (define-test "main"
-  )
+  (flet ((basic-tests (system)
+           (flet ((check-unbound ()
+                    (is eq nil
+                        (defsys:locate system 'key :errorp nil))
+                    (fail (defsys:locate system 'key))
+                    (is eq nil
+                        (defsys:boundp system 'key))
+                    (is = 0
+                        (defsys:count system))))
+             (check-unbound)
+             (setf (defsys:locate system 'key) 'value)
+             (is eq 'value
+                 (defsys:locate system 'key))
+             (defsys:map (let ((count 0))
+                           (lambda (key value)
+                             (incf count)
+                             (are equal '(1 key value)
+                                  (values count key value))))
+                         system)
+             (defsys:unbind system 'key)
+             (check-unbound))))
+    (basic-tests (make-instance 'defsys:hash-table-mixin))
+    (basic-tests (make-instance 'defsys:standard-system :base-definition-class t))))
+
+; base-definition-class-mixin
+; primary-binding-mixin
+; alias-bindings-mixin
+; map-aliasing-systems
+; ensure, expand, define, map, count
+; simple-expansion-mixin
